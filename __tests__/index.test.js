ee49d93aa05cf729469f9edc4236682541464a2a
Mylanych <zotov_vadim@inbox.ru>
2021-07-11 22:20:28 +0300

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,5 +2,5 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 test('genDiff', () => {
-  expect(genDiff(6)).toBe(3);
+  expect(genDiff(file1, file2)).toBe(null);
 });
